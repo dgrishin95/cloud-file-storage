f633c5f3706f59e0f53f61b953e9d611d47ed409
Danila Grishin <spacecowboy790@gmail.com>
2025-10-18 13:48:40 +0700

    Initial commit

---
+++ src/main/java/com/mysite/cloud_file_storage/CloudFileStorageApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.cloud_file_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFileStorageApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CloudFileStorageApplication.class, args);
+    }
+
+}
